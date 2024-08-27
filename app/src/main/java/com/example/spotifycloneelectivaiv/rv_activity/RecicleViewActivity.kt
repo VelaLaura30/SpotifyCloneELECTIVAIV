a4551e773861fabb32e66f5ed2ebb6d88ab47ef1
VelaLaura30 <laura.vela03@uptc.edu.co>
2024-08-27 17:52:04 -0500

    Recycle View Spotify UI

---
+++ app/src/main/java/com/example/spotifycloneelectivaiv/rv_activity/RecicleViewActivity.kt
@@ -0,0 +1,39 @@
+package com.example.spotifycloneelectivaiv.rv_activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.spotifycloneelectivaiv.R
+import com.example.spotifycloneelectivaiv.SongHome
+import com.example.spotifycloneelectivaiv.rv_activity.adapters.RVAdapterSongs
+
+class RecyclerViewActivity : AppCompatActivity() {
+
+    private lateinit var rvSongs: RecyclerView
+    private val songList = arrayListOf<SongHome>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+        fillSongList()
+        initViews()
+    }
+
+    private fun initViews() {
+        rvSongs = findViewById(R.id.rv_songs)
+        rvSongs.apply {
+            layoutManager = LinearLayoutManager(this@RecyclerViewActivity, LinearLayoutManager.VERTICAL, false)
+            adapter = RVAdapterSongs(songList)
+        }
+    }
+
+    private fun fillSongList() {
+        for (i in 0 until 10) {
+            val songName = "Nombre Canción $i"
+            val artistName = "Artista X"
+            val info = "$songName - $artistName"
+            songList.add(SongHome(info))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/spotifycloneelectivaiv/rv_activity/adapters/RVAdapterSongs.kt
@@ -0,0 +1,11 @@
+package com.example.spotifycloneelectivaiv.rv_activity.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.spotifycloneelectivaiv.SongHome
+
+class RVAdapterSongs (private val songs: List<SongHome>):
+RecyclerView.Adapter<RVAdapterSongs.SongViewHolder>() {
+
+    class SongViewHolder()
+
+}
